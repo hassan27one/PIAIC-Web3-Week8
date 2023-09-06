e15c61884bfdc525f93df9d6a6895a0a269f38c1
hassan27one <143028617+hassan27one@users.noreply.github.com>
2023-09-06 11:44:19 +0500

    Q5

--- week8.js
+++ week8.js
@@ -1,10 +1,50 @@
 "use strict";
 // Hassan Farooq  Web-3 PIAIC57456   NTU Faisalabad
 //Week-08  Assignment-08    06-Sep-2023
-// Question- 1- Install Node.js, TypeScript and VS Code on your computer.
+console.log("Question-1 ================================================================");
+// Question-1- Install Node.js, TypeScript and VS Code on your computer.
 // Installed Node js from website and check from command prompt type cmd >> node -v version =20.4
 // Installed Typescript from command prompt type in search = cmd >> npm install -g typescript then type tsc -v version =5.1.6
 // Installed VSCode from website and click next and setup then finish from command prompt type cmd >> code -v version =1.8
-// Question- 2 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+console.log("Question-2 ================================================================");
+// Question-2 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 var n1 = "Hassan Farooq";
 console.log("“Hello Mr. ", n1, " Would you like to learn some Typescript today?");
+console.log("Question-3 ================================================================");
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var n3u = "HASSAN FAROOQ";
+console.log("Name toLowerCase = ", n3u.toLowerCase());
+var n3L = "hassan farooq";
+console.log("Name toUpperCase = ", n3L.toUpperCase());
+var n3 = "hassan farooq";
+// console.log("Name totitleCase = ", n3.totitleCase());
+console.log("Question-4 ================================================================");
+// Famous Quote: Find a quote from a famous person you admire. 
+// Print the quote and the name of its author. 
+// Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var quotation = "A person who never made a mistake never tried anything new.";
+var author = "Albert Einstein ";
+console.log(author, " said, ", quotation);
+console.log("Question-5 ================================================================");
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Albert Einstein";
+var message = " my message is you shoud try work for new things regardless of failure othervise you cannnot able to create something new";
+console.log(famous_person, " said, ", message);
+console.log("Question-6 ================================================================");
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+console.log("Question-7 ================================================================");
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log("Question-8 ================================================================");
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log("Question-9 ================================================================");
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+console.log("Question-10 ================================================================");
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+console.log("Question-11 ================================================================");
+console.log("Question-12 ================================================================");
+// Question-3 ==========================================================================
+// Question-3 ==========================================================================
