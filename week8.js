87adb647536fb8db6de262359e8497e82fabc7ad
hassan27one <143028617+hassan27one@users.noreply.github.com>
2023-09-06 10:12:26 +0500

    Week08

---
+++ week8.js
@@ -0,0 +1,32 @@
+"use strict";
+// we want to save some Action of this Objects  
+let car = [
+    {
+        Model: "city VXi",
+        Brand: "Honda",
+        Year: 2013,
+        color: "white",
+        race: () => {
+            return "Racing Racing ";
+        }
+    },
+    {
+        Model: "C454",
+        Brand: "BMW",
+        Year: 2021,
+        color: "Black",
+        race: () => {
+            return "Racing-2 Racing-2 ";
+        }
+    }
+];
+console.log(car[1].race());
+// import inquirer from "inquirer";
+// const data = await inquirer.prompt(
+//     [{
+//         name:"num",
+//         type:"number",
+//         message:"Enater a Message ", 
+//     }]
+// );
+// npm install data 
