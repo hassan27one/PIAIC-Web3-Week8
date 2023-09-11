22e0582916cbf52e131191e84d543f934a64b944
hassan27one <143028617+hassan27one@users.noreply.github.com>
2023-09-11 21:31:45 +0500

    Solved upto Q31

--- week8.ts
+++ week8.ts
@@ -557,6 +557,13 @@ for (let i = 0; i < usernames.length; i++)
 	else {  console.log("Hello " + i + ", thank you for logging in again!");  }
 }
 
+console.log("Question-31 ================================================================ \n");
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+
+// • If the list is empty, print the message We need to find some users!
+
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
 
 
 
