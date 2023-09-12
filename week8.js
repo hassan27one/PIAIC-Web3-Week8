2b29683ff393ba107fa9b215ce63c03a87b7baf7
hassan27one <143028617+hassan27one@users.noreply.github.com>
2023-09-13 01:04:29 +0500

    Solved upto Q39

--- week8.js
+++ week8.js
@@ -534,5 +534,16 @@ describe_city('Atlanta');
 describe_city(' Las Vagas ');
 describe_city(' Taxes ');
 describe_city('paris', 'France');
+console.log("Question-39 ================================================================ \n");
+// City Names: Write a function called city_country() that takes in the name of a city and its country. 
+// The function should return a string formatted like this: "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+let city_country = (city, country) => {
+    return (city + " , " + country);
+};
+console.log(city_country('Lahore', 'Pakistan'));
+console.log(city_country('Dhaka ', 'Bangladash'));
+console.log(city_country('Delhi ', 'India'));
+console.log(city_country('paris', 'France'));
 // Question-3 ==========================================================================
 // Question-3 ==========================================================================
