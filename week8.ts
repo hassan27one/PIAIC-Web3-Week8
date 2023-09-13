63b239aaddf6b49ef8538e3b1c8e552f94531acc
hassan27one <143028617+hassan27one@users.noreply.github.com>
2023-09-13 20:26:08 +0500

    Solved upto Q40

--- week8.ts
+++ week8.ts
@@ -699,7 +699,15 @@ let city_country = (city:string, country:string) => {
 console.log(city_country('Lahore', 'Pakistan'))
 console.log(city_country('Dhaka ', 'Bangladash'))
 console.log(city_country('Delhi ', 'India'))
-console.log(city_country('paris',  'France'))
+console.log(city_country('paris',  'France \n'))
+
+console.log("Question-40 ================================================================ \n");
+// Album: Write a function called make_album() that builds a Object describing a music album. 
+// The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. 
+// Use the function to make three dictionaries representing different albums. 
+// Print each return value to show that Objects are storing the album information correctly. 
+// Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. 
+// Make at least one new function call that includes the number of tracks on an album.
 
 
 
