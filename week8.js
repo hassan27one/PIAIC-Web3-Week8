99e9cee517ff7d8359a0ef4675099290c7c4e692
hassan27one <143028617+hassan27one@users.noreply.github.com>
2023-09-13 22:00:44 +0500

    Solved upto Q43

--- week8.js
+++ week8.js
@@ -574,5 +574,8 @@ function make_great(Magicians) {
 const great_magicians_names = make_great(MagicianNames);
 console.log("\n All Magicians = " + show_magicians(MagicianNames));
 console.log("\n Great Magicians = " + show_magicians(great_magicians_names));
-// Question-3 ==========================================================================
-// Question-3 ==========================================================================
+console.log("Question-43 ================================================================ \n");
+// Unchanged Magicians: Start with your work from Exercise 40. 
+// Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, 
+// return the new array and store it in a separate array. 
+// Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
--- week8.ts
+++ week8.ts
@@ -739,9 +739,10 @@ function make_great(Magicians:string[]) {
     
     console.log("\n Great Magicians = "+ show_magicians(great_magicians_names) );
 
+console.log("Question-43 ================================================================ \n");
+// Unchanged Magicians: Start with your work from Exercise 40. 
+// Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, 
+// return the new array and store it in a separate array. 
+// Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
 
 
-
-// Question-3 ==========================================================================
-
-// Question-3 ==========================================================================
